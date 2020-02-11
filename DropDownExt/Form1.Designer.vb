ff9db53df1ffa08e86aef4f94efe6c2c6afcb23f
Marc <marcalfaro@live.com>
2020-02-11 23:41:13 +0800

    Add project files.

---
+++ DropDownExt/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'dgv
+        '
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(12, 65)
+        Me.dgv.Name = "dgv"
+        Me.dgv.Size = New System.Drawing.Size(776, 373)
+        Me.dgv.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(218, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.dgv)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ DropDownExt/Form1.vb
@@ -0,0 +1,72 @@
+﻿Option Explicit On
+
+Public Class Form1
+
+    Dim acColors As New AutoCompleteStringCollection()
+    Dim acAnimal As New AutoCompleteStringCollection()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        acColors.AddRange(New String() {"Red", "Orange", "Red Orange", "Blue", "Green", "Blue Green", "Black", "White"})
+        acAnimal.AddRange(New String() {"Zebra Fish", "Maine Coon", "Belgian Malinois", "Bull Dog", "Lazy Dog", "Goldfish", "Bull", "Cat"})
+
+        Build_DGV()
+
+        TextBox1.AutoCompleteMode = AutoCompleteMode.Suggest
+        TextBox1.AutoCompleteSource = AutoCompleteSource.CustomSource
+        TextBox1.AutoCompleteCustomSource = acAnimal
+
+    End Sub
+
+
+
+    Private Sub Build_DGV()
+        Try
+            With dgv.Columns
+                .Add(New DataGridViewTextBoxColumn With {.Name = "colColor", .HeaderText = "Color"})
+                .Add(New DataGridViewTextBoxColumn With {.Name = "colPet", .HeaderText = "Pet"})
+            End With
+
+        Catch ex As Exception
+            Debug.Print(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub dgv_EditingControlShowing(sender As Object, e As DataGridViewEditingControlShowingEventArgs) Handles dgv.EditingControlShowing
+        If TypeOf e.Control Is TextBox Then
+            Dim tb As TextBox = CType(e.Control, TextBox)
+
+            If Not IsNothing(tb) Then
+                tb.AutoCompleteMode = AutoCompleteMode.None
+                tb.AutoCompleteSource = AutoCompleteSource.None
+                tb.AutoCompleteCustomSource = Nothing
+
+                Select Case dgv.CurrentCell.ColumnIndex
+                    Case 0
+                        If acColors IsNot Nothing Then
+                            tb.AutoCompleteMode = AutoCompleteMode.Suggest
+                            tb.AutoCompleteSource = AutoCompleteSource.CustomSource
+                            tb.AutoCompleteCustomSource = acColors
+                        Else
+                            tb = Nothing
+                        End If
+
+                    Case 1
+                        If acAnimal IsNot Nothing Then
+                            tb.AutoCompleteMode = AutoCompleteMode.Suggest
+                            tb.AutoCompleteSource = AutoCompleteSource.CustomSource
+                            tb.AutoCompleteCustomSource = acAnimal
+                        Else
+                            tb = Nothing
+                        End If
+
+                End Select
+
+
+
+            End If
+
+        End If
+
+    End Sub
+End Class
